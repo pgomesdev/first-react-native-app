9fdb936ec5d2fcca3eed508a358591392c39352c
pgomesdev <gomes.pedro15@gmail.com>
2019-01-07 14:36:38 -0300

    ui: created stylesheets for components

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { createBottomTabNavigator, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import AddEntry from './components/AddEntry'
+import History from './components/History'
 import reducer from './reducers'
 import middleware from './middleware'
+import { purple, white } from './utils/colors'
 
 const store = createStore(reducer, middleware)
 
-export default class App extends React.Component {
+const TabsConfig = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      //tabBarIcon: ({ tintColor }) => <Ionicons name='plus-square' size={30} color={tintColor} />,
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      //tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />,
+    },
+  },
+}
+const TabsOptions = {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+  },
+  style: {
+    height: 56,
+    backgroundColor: Platform.OS === 'ios' ? white : purple,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 1,
+  },
+}
+
+// const Tabs = Platform.OS === 'ios'
+//   ? createBottomTabNavigator(TabsConfig, TabsOptions)
+//   : createMaterialTopTabNavigator(TabsConfig, TabsOptions)
+const Tabs = createMaterialTopTabNavigator(TabsConfig, TabsOptions)
+
+const TabsContainer = createAppContainer(Tabs)
+
+class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View>
-          <AddEntry />
+        <View style={{flex: 1}}>
+          <View style={{height: 20}} />
+          <TabsContainer />
         </View>
       </Provider>
     )
   }
 }
+
+export default App
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { connect } from 'react-redux'
 import DateHeader from './DateHeader'
@@ -9,13 +9,15 @@ import UdaciSteppers from './UdaciSteppers'
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import { submitEntry, removeEntry } from '../utils/api'
 import { addEntry } from '../actions'
+import { white, purple } from '../utils/colors'
 
 function SubmitBtn({ onPress }) {
   return (
     <TouchableOpacity
+      style={Platform.OS = 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
       onPress={onPress}
     >
-      <Text>SUBMIT</Text>
+      <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   )
 }
@@ -111,10 +113,10 @@ class AddEntry extends Component {
 
     if (this.props.alreadyLogged) {
       return (
-        <View>
-          <Ionicons name='md-happy' size={100} />
+        <View style={styles.center}>
+          <Ionicons name={Platform.OS = 'ios' ? 'ios-happy' : 'md-happy'} size={100} />
           <Text>You already logged your information for today.</Text>
-          <TextButton onPress={this.reset}>
+          <TextButton style={{padding: 10}} onPress={this.reset}>
             Reset
           </TextButton>
         </View>
@@ -122,14 +124,14 @@ class AddEntry extends Component {
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={new Date().toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === 'slider'
                 ? <UdaciSlider
@@ -152,6 +154,50 @@ class AddEntry extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  }
+})
 
 const mapStateToProps = (state) => {
   const key = timeToString()
